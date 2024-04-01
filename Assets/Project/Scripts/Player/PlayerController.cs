dddd697eadf55b65a6bdcf4f982edf84bb11eb33
Turgut Deniz Aysen <denizaysen005@gmail.com>
2024-04-01 19:23:47 +0300

    Movement init

--- Assets/Project/Scripts/Player/PlayerController.cs
+++ Assets/Project/Scripts/Player/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private int speed;
+
     private PlayerControls _playerControls;
+    private Rigidbody _rigidbody;
+    private Vector3 _movement;
     private float _x,_z;
     private void Awake()
     {
@@ -14,10 +18,20 @@ public class PlayerController : MonoBehaviour
     {
         _playerControls.Enable();
     }
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
     // Update is called once per frame
     void Update()
     {
         _x = _playerControls.Player.Move.ReadValue<Vector2>().x;
         _z = _playerControls.Player.Move.ReadValue<Vector2>().y;
+
+        _movement = new Vector3(_x,0,_z).normalized;
+    }
+    private void FixedUpdate()
+    {
+        _rigidbody.MovePosition(transform.position + _movement * speed * Time.fixedDeltaTime);
     }
 }
