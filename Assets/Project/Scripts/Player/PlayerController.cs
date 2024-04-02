5e10bf8ad3ad4fd763fdb7a383362acc80a08d8b
Turgut Deniz Aysen <denizaysen005@gmail.com>
2024-04-02 17:27:26 +0300

    Animations Integreated

--- Assets/Project/Scripts/Player/PlayerController.cs
+++ Assets/Project/Scripts/Player/PlayerController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private int speed;
+    [SerializeField] private Animator anim;
+    [SerializeField] private SpriteRenderer playerSprite;
 
     private PlayerControls _playerControls;
     private Rigidbody _rigidbody;
     private Vector3 _movement;
     private float _x,_z;
+
+    private const string IS_WALK = "IsWalk";
     private void Awake()
     {
         _playerControls = new PlayerControls();
@@ -29,6 +33,17 @@ public class PlayerController : MonoBehaviour
         _z = _playerControls.Player.Move.ReadValue<Vector2>().y;
 
         _movement = new Vector3(_x,0,_z).normalized;
+
+        anim.SetBool(IS_WALK, _movement != Vector3.zero);
+
+        if(_x != 0 && _x < 0)
+        {
+            playerSprite.flipX = true;
+        }
+        if(_x != 0 && _x > 0)
+        {
+            playerSprite.flipY = false;
+        }
     }
     private void FixedUpdate()
     {
